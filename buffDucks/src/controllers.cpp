b9592c02f5d4e4260cddbf877b3ac9441219f3cd
Singhzing-kid1 <virsheeng@protonmail.com>
2023-10-15 23:02:43 -0600

    fixed some bad logic i think

--- buffDucks/src/controllers.cpp
+++ buffDucks/src/controllers.cpp
@@ -6,14 +6,18 @@ using namespace pros;
 Controller master (E_CONTROLLER_MASTER);
 
 int32_t deadzone(controller_analog_e_t valueY, controller_analog_e_t valueX){
-    if ((master.get_analog(valueY) >= 27 || master.get_analog(valueY) <= -27) && master.get_analog(valueX) == 0){
+    if ((master.get_analog(valueY) >= 27 || master.get_analog(valueY) <= -27) && master.get_analog(valueX) == 0){ // some ugly computation to figure out whether or 
         return master.get_analog(valueY);
     } else if ((master.get_analog(valueY) >= 27 || master.get_analog(valueY) <= -27) && master.get_analog(valueX) != 0){
-       if(master.get_analog(valueY) < 0){
-        return (int32_t)-hypot((double)master.get_analog(valueX), (double)master.get_analog(valueY));
-       } else if(master.get_analog(valueY) > 0){
-        return (int32_t)hypot((double)master.get_analog(valueX), (double)master.get_analog(valueY));
-       }
+        if(master.get_analog(valueY) < 0){
+            if(-hypot((double)master.get_analog(valueX), (double)master.get_analog(valueY)) <= -27){
+                return (int32_t)-hypot((double)master.get_analog(valueX), (double)master.get_analog(valueY));
+            }
+        } else if(master.get_analog(valueY) > 0){
+            if(hypot((double)master.get_analog(valueX), (double)master.get_analog(valueY)) >= 27){
+                return (int32_t)hypot((double)master.get_analog(valueX), (double)master.get_analog(valueY));
+            }
+        }
     } else {
         return (int32_t)0;
     }
